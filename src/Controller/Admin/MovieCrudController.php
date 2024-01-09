c3727409e2bed959040d3821020cb9692438a226
LennyB <tenruz@hotmail.com>
2024-01-09 13:55:14 +0100

    fin session 4

---
+++ src/Controller/Admin/MovieCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Movie;
+use DateTime;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class MovieCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Movie::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('name','Nom');
+        yield DateField::new('release_date','Date de sortie');
+        yield AssociationField::new('directors','Réalisateur');
+        yield AssociationField::new('genres','Genre');
+    }
+    
+}
