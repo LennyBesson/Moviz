668a652f05e37eb5391273f2f11d3e8f558dfb36
LennyB <tenruz@hotmail.com>
2024-01-10 18:38:31 +0100

    fin session 6

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Entity\Review;
+use App\Form\ReviewType;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
+    #[Route('/movie', name: 'app_movies')]
     public function index(MovieRepository $movieRepository): Response
     {
         $movies = $movieRepository->findBy([],['id' => 'DESC']);
@@ -19,6 +21,19 @@ class MovieController extends AbstractController
 
         return $this->render('movie/index.html.twig', [
             'movies' => $movies,
+        ]);
+    }
+
+
+    #[Route('/movie/{id}', name: 'app_movie_show')]
+    public function show(Movie $movie): Response
+    {
+        $review = new Review();
+        $form = $this->createForm(ReviewType::class, $review);
+        
+        return $this->render('movie/show.html.twig', [
+            'movie' => $movie,
+            'form' => $form,
         ]);
     }
 }
