6fe8d890007db8069e3347dc9f33cfdc06e78b1d
LennyB <tenruz@hotmail.com>
2024-01-11 14:52:30 +0100

    fin de session 7

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -6,16 +6,22 @@ use App\Entity\Movie;
 use App\Entity\Review;
 use App\Form\ReviewType;
 use App\Repository\MovieRepository;
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
     #[Route('/movie', name: 'app_movies')]
-    public function index(MovieRepository $movieRepository): Response
+    public function index(MovieRepository $movieRepository, Request $request): Response
     {
-        $movies = $movieRepository->findBy([],['id' => 'DESC']);
+        $genreId = $request->get('genreId');
+        $movies = $movieRepository->findMovie($genreId);
 
         dump($movies);
 
@@ -26,14 +32,34 @@ class MovieController extends AbstractController
 
 
     #[Route('/movie/{id}', name: 'app_movie_show')]
-    public function show(Movie $movie): Response
+    public function show(Movie $movie, Request $request, EntityManagerInterface $entityManager, Security $security, ReviewRepository $reviewRepository): Response
     {
-        $review = new Review();
+        $averageRate = $reviewRepository->getAverageRateByMovieId($movie->getId());
+
+        $user = $security->getUser();
+
+        $review = $reviewRepository->findOneBy(['movie'=> $movie, 'user' => $user]);
+
+        if(!$review) {
+            $review = new Review();
+            $review->setMovie($movie);
+            $review->setUser($user);
+            $review->setApproved(false);
+        }
+
         $form = $this->createForm(ReviewType::class, $review);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($review);
+            $entityManager->flush();
+        }
         
         return $this->render('movie/show.html.twig', [
             'movie' => $movie,
             'form' => $form,
+            'user' => $user,
+            'averageRate' => $averageRate,
         ]);
     }
 }
